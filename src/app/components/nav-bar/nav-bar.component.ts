9737c7fd55d94b4f1bc14f7761d6f40ee73e6d4f
Chathurika <chathuswijekoonuom@gmail.com>
2023-07-21 00:21:23 +0530

    feat: :sparkles: Set Navigation

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-nav-bar',
@@ -6,8 +7,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.scss'],
 })
 export class NavBarComponent implements OnInit {
-  constructor() {}
+  constructor(private viewportScroller: ViewportScroller) {}
 
   ngOnInit(): void {}
 
+  navigateToSection(section: string) {
+    if (section == 'skills') {
+      const skillsSectionRef = document.getElementById('skillsSection');
+      if (skillsSectionRef) {
+        this.viewportScroller.scrollToAnchor('skillsSection');
+      }
+    } else if (section == 'services') {
+      const servicesSectionRef = document.getElementById('servicesSection');
+      if (servicesSectionRef) {
+        this.viewportScroller.scrollToAnchor('servicesSection');
+      }
+    } else if (section == 'timeline') {
+      const timelineSectionRef = document.getElementById('timelineSection');
+      if (timelineSectionRef) {
+        this.viewportScroller.scrollToAnchor('timelineSection');
+      }
+    } else if (section == 'testimonials') {
+      const testimonialsSectionRef = document.getElementById(
+        'testimonialsSection'
+      );
+      if (testimonialsSectionRef) {
+        this.viewportScroller.scrollToAnchor('testimonialsSection');
+      }
+    } else if (section == 'projects') {
+      const projectsSectionRef = document.getElementById('projectsSection');
+      if (projectsSectionRef) {
+        this.viewportScroller.scrollToAnchor('projectsSection');
+      }
+    } else if (section == 'contact') {
+      const contactSectionRef = document.getElementById('contactSection');
+      if (contactSectionRef) {
+        this.viewportScroller.scrollToAnchor('contactSection');
+      }
+    }
+  }
 }
